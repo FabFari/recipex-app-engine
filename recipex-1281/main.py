5b136475b9d7a26da0f96d5b46d15c1380f4b38a
FabFari <fabriziofarinacci@gmail.com>
2016-04-17 17:34:49 +0100

    Added Add and Update method for Measurement, Measurement table updated.

--- recipex-1281/main.py
+++ recipex-1281/main.py
@@ -24,8 +24,16 @@ from google.appengine.ext import ndb
 
 from datetime import datetime
 
+import logging
 
-# Datastore Classes
+
+# CONSTANTS
+
+MEASUREMENTS_KIND = ["BP", "HR", "RR", "SpO2", "HGT", "TMP", "PAIN", "CHL"]
+
+
+# DATASTORE CLASSES
+
 class User(ndb.Model):
     email = ndb.StringProperty(required=True)
     name = ndb.StringProperty(required=True)
@@ -35,7 +43,7 @@ class User(ndb.Model):
     city = ndb.StringProperty()
     address = ndb.StringProperty()
     relatives = ndb.KeyProperty(repeated=True)
-    pc_phycian = ndb.KeyProperty()
+    pc_physician = ndb.KeyProperty()
     visiting_nurse = ndb.KeyProperty()
     caregivers = ndb.KeyProperty(repeated=True)
 
@@ -53,7 +61,6 @@ class Measurement(ndb.Model):
     # Blood Pressure (BP)
     systolic = ndb.IntegerProperty()
     diastolic = ndb.IntegerProperty()
-    pulse = ndb.IntegerProperty()
     # Heart Rate (HR)
     bpm = ndb.IntegerProperty()
     # Respiratory Rate (RR)
@@ -62,17 +69,18 @@ class Measurement(ndb.Model):
     spo2 = ndb.FloatProperty()
     # Blood Sugar (HGT)
     hgt = ndb.FloatProperty()
-    # Body Temperature (T)
+    # Body Temperature (TMP)
     degrees = ndb.FloatProperty()
     # Pain (P)
     nrs = ndb.IntegerProperty()
-    # Cholesterol
-    level = ndb.IntegerProperty()
+    # Cholesterol (CHL)
+    chl_level = ndb.IntegerProperty()
 
 
-# Message Classes
+# MESSAGE CLASSES
+
 class DefaultResponseMessage(messages.Message):
-    code = messages.IntegerField(1)
+    code = messages.StringField(1)
     message = messages.StringField(2)
     payload = messages.StringField(3)
 
@@ -90,7 +98,7 @@ class RegisterUserMessage(messages.Message):
 
 
 class UpdateUserMessage(messages.Message):
-    id = messages.StringField(1, required=True)
+    id = messages.IntegerField(1, required=True)
     name = messages.StringField(2)
     surname = messages.StringField(3)
     birth = messages.StringField(4)
@@ -102,7 +110,7 @@ class UpdateUserMessage(messages.Message):
 
 
 class UserIdMessage(messages.Message):
-    id = messages.StringField(1, required=True)
+    id = messages.IntegerField(1, required=True)
 
 
 class UserInfoMessage(messages.Message):
@@ -117,6 +125,51 @@ class UserInfoMessage(messages.Message):
     years_exp = messages.IntegerField(9)
     response = messages.MessageField(DefaultResponseMessage, 10)
 
+
+class AddMeasurementMessage(messages.Message):
+    user_id = messages.IntegerField(1, required=True)
+    date_time = messages.StringField(2, required=True)
+    kind = messages.StringField(3, required=True)
+    # Blood Pressure (BP)
+    systolic = messages.IntegerField(4)
+    diastolic = messages.IntegerField(5)
+    # Heart Rate (HR)
+    bpm = messages.IntegerField(6)
+    # Respiratory Rate (RR)
+    respirations = messages.IntegerField(7)
+    # Pulse Oximetry (SpO2)
+    spo2 = messages.FloatField(8)
+    # Blood Sugar (HGT)
+    hgt = messages.FloatField(9)
+    # Body Temperature (T)
+    degrees = messages.FloatField(10)
+    # Pain (P)
+    nrs = messages.IntegerField(11)
+    # Cholesterol
+    chl_level = messages.IntegerField(12)
+
+class UpdateMeasurementMessage(messages.Message):
+    id = messages.IntegerField(1, required=True)
+    user_id = messages.IntegerField(2, required=True)
+    date_time = messages.StringField(3, required=True)
+    kind = messages.StringField(4, required=True)
+    # Blood Pressure (BP)
+    systolic = messages.IntegerField(5)
+    diastolic = messages.IntegerField(6)
+    # Heart Rate (HR)
+    bpm = messages.IntegerField(7)
+    # Respiratory Rate (RR)
+    respirations = messages.IntegerField(8)
+    # Pulse Oximetry (SpO2)
+    spo2 = messages.FloatField(9)
+    # Blood Sugar (HGT)
+    hgt = messages.FloatField(10)
+    # Body Temperature (T)
+    degrees = messages.FloatField(11)
+    # Pain (P)
+    nrs = messages.IntegerField(12)
+    # Cholesterol
+    chl_level = messages.IntegerField(13)
 
 @endpoints.api(name="recipexServerApi", version="v1",
                hostname="recipex-1281.appspot.com",
@@ -137,7 +190,11 @@ class RecipexServerApi(remote.Service):
         if User.query(User.email == request.email).count() > 0:
             return DefaultResponseMessage(code="412 Precondition Failed", message="User already existent.")
 
-        birth = datetime.strptime(request.birth, "%Y-%m-%d")
+        try:
+            birth = datetime.strptime(request.birth, "%Y-%m-%d")
+        except ValueError:
+            return DefaultResponseMessage(code="400 Bad Request", message="Bad birth format.")
+
         new_user = User(email=request.email, name=request.name, surname=request.surname, birth=birth,
                         sex=request.sex, city=request.city, address=request.address)
         user_key = new_user.put()
@@ -146,13 +203,13 @@ class RecipexServerApi(remote.Service):
             new_caregiver = Caregiver(parent=user_key, field=request.field, years_exp=request.years_exp)
             new_caregiver.put()
 
-        return DefaultResponseMessage(code="201 Created", message="User registered.", payload=user_key.urlsafe())
+        return DefaultResponseMessage(code="201 Created", message="User registered.", payload=user_key.id())
 
     @endpoints.method(UpdateUserMessage, DefaultResponseMessage,
                       path="users/{id}", http_method="PUT", name="user.updateUser")
     def update_user(self, request):
         RecipexServerApi.authentication_check()
-        user = Key(urlsafe=request.id).get()
+        user = Key(User, int(request.id)).get()
         if not user:
             return DefaultResponseMessage(code="404 Not Found", message="Failed to update user info.")
 
@@ -197,10 +254,10 @@ class RecipexServerApi(remote.Service):
                       path="users/{id}", http_method="GET", name="user.getUser")
     def get_user(self, request):
         RecipexServerApi.authentication_check()
-        user = Key(urlsafe=request.id).get()
+        user = Key(User, int(request.id)).get()
         if not user:
-            return UserInfoMessage(response=DefaultResponseMessage(code="404 Not Found",
-                                                                   message="Failed to get user info."))
+            return DefaultResponseMessage(code="404 Not Found", message="Failed to update user info.")
+
         birth = datetime.strftime(user.birth, "%Y-%m-%d")
 
         usr_info = UserInfoMessage(email=user.email, name=user.name, surname=user.surname,
@@ -219,11 +276,169 @@ class RecipexServerApi(remote.Service):
                       path="users/{id}", http_method="DELETE", name="user.deleteUser")
     def delete_user(self, request):
         RecipexServerApi.authentication_check()
-        user = Key(urlsafe=request.id).get()
+        user = Key(User, int(request.id)).get()
         if not user:
-            return DefaultResponseMessage(code="404 Not Found", message="User not existent.")
+            return DefaultResponseMessage(code="404 Not Found", message="Failed to update user info.")
+
         user.key.delete()
         return DefaultResponseMessage(code="200 OK", message="User deleted.")
+
+    @endpoints.method(AddMeasurementMessage, DefaultResponseMessage,
+                      path="users/{user_id}/measurements", http_method="POST", name="measurements.addMeasurement")
+    def add_measurement(self, request):
+        RecipexServerApi.authentication_check()
+
+        user = Key(User, request.user_id).get()
+        if not user:
+            return DefaultResponseMessage(code="404 Not Found", message="User not existent.")
+
+        try:
+            date_time = datetime.strptime(request.date_time, "%Y-%m-%d %H:%M:%S")
+        except ValueError:
+            return DefaultResponseMessage(code="400 Bad Request", message="Bad date_time format.")
+
+        if request.kind not in MEASUREMENTS_KIND:
+            return DefaultResponseMessage(code="412 Precondition Failed", message="Wrong measurement kind.")
+
+        new_measurement = Measurement(parent=user.key, date_time=date_time, kind=request.kind)
+
+        if request.kind == "BP":
+            if not request.systolic or not request.diastolic:
+                return DefaultResponseMessage(code="412 Precondition Failed", message="Input parameter(s) missing.")
+            if (request.systolic < 0 or request.systolic > 250) or (request.diastolic < 0 or request.diastolic > 250):
+                return DefaultResponseMessage(code="412 Precondition Failed",
+                                              message="Input parameter(s) out of range.")
+            new_measurement.systolic = request.systolic
+            new_measurement.diastolic = request.diastolic
+        elif request.kind == "HR":
+            if not request.bpm:
+                return DefaultResponseMessage(code="412 Precondition Failed", message="Input parameter missing.")
+            if request.bpm < 0 or request.bpm > 400:
+                return DefaultResponseMessage(code="412 Precondition Failed",
+                                              message="Input parameter out of range.")
+            new_measurement.bpm = request.bpm
+        elif request.kind == "RR":
+            if not request.respirations:
+                return DefaultResponseMessage(code="412 Precondition Failed", message="Input parameter missing.")
+            if request.respirations < 0 or request.respirations > 200:
+                return DefaultResponseMessage(code="412 Precondition Failed",
+                                              message="Input parameter out of range.")
+            new_measurement.respirations = request.respirations
+        elif request.kind == "SpO2":
+            if not request.spo2:
+                return DefaultResponseMessage(code="412 Precondition Failed", message="Input parameter missing.")
+            if request.spo2 < 0 or request.spo2 > 100:
+                return DefaultResponseMessage(code="412 Precondition Failed",
+                                              message="Input parameter out of range.")
+            new_measurement.spo2 = request.spo2
+        elif request.kind == "HGT":
+            if not request.hgt:
+                return DefaultResponseMessage(code="412 Precondition Failed", message="Input parameter missing.")
+            if request.hgt < 0 or request.hgt > 600:
+                return DefaultResponseMessage(code="412 Precondition Failed",
+                                              message="Input parameter out of range.")
+            new_measurement.hgt = request.hgt
+        elif request.kind == "TMP":
+            if not request.degrees:
+                return DefaultResponseMessage(code="412 Precondition Failed", message="Input parameter missing.")
+            if request.degrees < 30 or request.degrees > 45:
+                return DefaultResponseMessage(code="412 Precondition Failed",
+                                              message="Input parameter out of range.")
+            new_measurement.degrees = request.degrees
+        elif request.kind == "PAIN":
+            if not request.nrs:
+                return DefaultResponseMessage(code="412 Precondition Failed", message="Input parameter missing.")
+            if request.nrs < 0 or request.hgt > 10:
+                return DefaultResponseMessage(code="412 Precondition Failed",
+                                              message="Input parameter out of range.")
+            new_measurement.nrs = request.nrs
+        else:
+            if not request.chl_level:
+                return DefaultResponseMessage(code="412 Precondition Failed", message="Input parameter missing.")
+            if request.chl_level < 0 or request.chl_level > 800:
+                return DefaultResponseMessage(code="412 Precondition Failed",
+                                              message="Input parameter out of range.")
+            new_measurement.chl_level = request.chl_level
+
+        measurement_key = new_measurement.put()
+
+        return DefaultResponseMessage(code="201 Created", message="Measurement added.", payload=measurement_key.id())
+
+    @endpoints.method(UpdateMeasurementMessage, DefaultResponseMessage,
+                      path="users/{user_id}/measurements/{id}", http_method="PUT", name="measurements.updateMeasurement")
+    def update_measurement(self, request):
+        measurement = Key(Measurement, request.id).get()
+        if not measurement:
+            return DefaultResponseMessage(code="404 Not Found", message="Measurement not existent.")
+        user_key = Key(User, request.user_id)
+        if user_key != measurement.parent:
+            return DefaultResponseMessage(code="401 Unauthorized", message="User unauthorized.")
+
+        try:
+            date_time = datetime.strptime(request.date_time, "%Y-%m-%d %H:%M:%S")
+            measurement.date_time = date_time
+        except ValueError:
+            return DefaultResponseMessage(code="400 Bad Request", message="Bad date_time format.")
+
+        if request.kind not in MEASUREMENTS_KIND:
+            return DefaultResponseMessage(code="412 Precondition Failed", message="Wrong measurement kind.")
+
+        if measurement.kind == "BP":
+            if request.systolic:
+                if request.systolic < 0 or request.systolic > 250:
+                    return DefaultResponseMessage(code="412 Precondition Failed",
+                                                  message="Input parameter out of range.")
+                measurement.systolic = request.systolic
+            if request.diastolic:
+                if request.diastolic < 0 or request.diastolic > 250:
+                    return DefaultResponseMessage(code="412 Precondition Failed",
+                                                  message="Input parameter out of range.")
+                measurement.diastolic = request.diastolic
+        elif request.kind == "HR":
+            if request.bpm:
+                if request.bpm < 0 or request.bpm > 400:
+                    return DefaultResponseMessage(code="412 Precondition Failed",
+                                                  message="Input parameter out of range.")
+                measurement.bpm = request.bpm
+        elif request.kind == "RR":
+            if request.respirations:
+                if request.respirations < 0 or request.respirations > 200:
+                    return DefaultResponseMessage(code="412 Precondition Failed",
+                                                  message="Input parameter out of range.")
+                measurement.respirations = request.respirations
+        elif request.kind == "SpO2":
+            if request.spo2:
+                if request.spo2 < 0 or request.spo2 > 100:
+                    return DefaultResponseMessage(code="412 Precondition Failed",
+                                                  message="Input parameter out of range.")
+                measurement.spo2 = request.spo2
+        elif request.kind == "HGT":
+            if request.hgt:
+                if request.hgt < 0 or request.hgt > 600:
+                    return DefaultResponseMessage(code="412 Precondition Failed",
+                                                  message="Input parameter out of range.")
+                measurement.hgt = request.hgt
+        elif request.kind == "TMP":
+            if request.degrees:
+                if request.degrees < 30 or request.degrees > 45:
+                    return DefaultResponseMessage(code="412 Precondition Failed",
+                                                  message="Input parameter out of range.")
+                measurement.degrees = request.degrees
+        elif request.kind == "PAIN":
+            if request.nrs:
+                if request.nrs < 0 or request.nrs > 10:
+                    return DefaultResponseMessage(code="412 Precondition Failed",
+                                                  message="Input parameter out of range.")
+                measurement.nrs = request.nrs
+        else:
+            if request.chl_level:
+                if request.chl_level < 0 or request.chl_level > 800:
+                    return DefaultResponseMessage(code="412 Precondition Failed",
+                                                  message="Input parameter out of range.")
+                measurement.chl_level = request.ch_level
+
+        measurement.put()
+        return DefaultResponseMessage(code="200 OK", message="Measurement updated.")
 
     @classmethod
     def authentication_check(cls):

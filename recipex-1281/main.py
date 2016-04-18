f3c2bd9af2cd894cd55ffbb3090d52211c3a34fa
FabFari <fabriziofarinacci@gmail.com>
2016-04-18 16:58:54 +0100

    Finished Measurement methods, added relatives/caregiver management methods, added TODOs

--- recipex-1281/main.py
+++ recipex-1281/main.py
@@ -42,13 +42,13 @@ class User(ndb.Model):
     sex = ndb.StringProperty()
     city = ndb.StringProperty()
     address = ndb.StringProperty()
-    relatives = ndb.KeyProperty(repeated=True)
+    relatives = ndb.PickleProperty(compressed=True)
     pc_physician = ndb.KeyProperty()
     visiting_nurse = ndb.KeyProperty()
-    caregivers = ndb.KeyProperty(repeated=True)
+    caregivers = ndb.PickleProperty(compressed=True)
 
 
-# Mettere disponibilita con Google Calendar
+# TODO Mettere disponibilita con Google Calendar
 class Caregiver(ndb.Model):
     field = ndb.StringProperty(required=True)
     years_exp = ndb.StringProperty()
@@ -171,6 +171,38 @@ class UpdateMeasurementMessage(messages.Message):
     # Cholesterol
     chl_level = messages.IntegerField(13)
 
+class MeasurementIdMessage(messages.Message):
+    user_id = messages.IntegerField(1, required=True)
+    id = messages.IntegerField(2, required=True)
+
+class MeasurementInfoMessage(messages.Message):
+    date_time = messages.StringField(1)
+    kind = messages.StringField(2)
+    # Blood Pressure (BP)
+    systolic = messages.IntegerField(3)
+    diastolic = messages.IntegerField(4)
+    # Heart Rate (HR)
+    bpm = messages.IntegerField(5)
+    # Respiratory Rate (RR)
+    respirations = messages.IntegerField(6)
+    # Pulse Oximetry (SpO2)
+    spo2 = messages.FloatField(7)
+    # Blood Sugar (HGT)
+    hgt = messages.FloatField(8)
+    # Body Temperature (T)
+    degrees = messages.FloatField(9)
+    # Pain (P)
+    nrs = messages.IntegerField(10)
+    # Cholesterol
+    chl_level = messages.IntegerField(11)
+    response = messages.MessageField(DefaultResponseMessage, 12)
+
+
+class UserRelativeCaregiverMessage(messages.Message):
+    id = messages.IntegerField(1, required=True)
+    set = messages.IntegerField(2, required=True, repeated=True)
+
+
 @endpoints.api(name="recipexServerApi", version="v1",
                hostname="recipex-1281.appspot.com",
                allowed_client_ids=[endpoints.API_EXPLORER_CLIENT_ID],
@@ -209,19 +241,9 @@ class RecipexServerApi(remote.Service):
                       path="users/{id}", http_method="PUT", name="user.updateUser")
     def update_user(self, request):
         RecipexServerApi.authentication_check()
-        user = Key(User, int(request.id)).get()
+        user = Key(User, request.id).get()
         if not user:
             return DefaultResponseMessage(code="404 Not Found", message="Failed to update user info.")
-
-        '''
-            relatives = request.relatives
-            relative_keys = []
-            if relatives:
-                for rel_key in range(len(relatives)):
-                    relative = Key(urlsafe=rel_key).get()
-                    if relative is not None:
-                        relative_keys.append(relative.key)
-        '''
 
         if request.name:
             user.name = request.name
@@ -254,12 +276,12 @@ class RecipexServerApi(remote.Service):
                       path="users/{id}", http_method="GET", name="user.getUser")
     def get_user(self, request):
         RecipexServerApi.authentication_check()
-        user = Key(User, int(request.id)).get()
+        user = Key(User, request.id).get()
         if not user:
-            return DefaultResponseMessage(code="404 Not Found", message="Failed to update user info.")
+            return DefaultResponseMessage(code="404 Not Found", message="User not existent.")
 
         birth = datetime.strftime(user.birth, "%Y-%m-%d")
-
+        # TODO Aggiungere ritorno parametri mancanti
         usr_info = UserInfoMessage(email=user.email, name=user.name, surname=user.surname,
                                    birth=birth, sex=user.sex, city=user.city, address=user.address,
                                    response=DefaultResponseMessage(code="200 OK",
@@ -276,12 +298,64 @@ class RecipexServerApi(remote.Service):
                       path="users/{id}", http_method="DELETE", name="user.deleteUser")
     def delete_user(self, request):
         RecipexServerApi.authentication_check()
-        user = Key(User, int(request.id)).get()
+        user = Key(User, request.id).get()
         if not user:
-            return DefaultResponseMessage(code="404 Not Found", message="Failed to update user info.")
-
+            return DefaultResponseMessage(code="404 Not Found", message="User not existent.")
+        # TODO Gestire le rimozioni in cascata (Misurazioni, Familiari, Caregiver, exc.)
         user.key.delete()
         return DefaultResponseMessage(code="200 OK", message="User deleted.")
+
+    @endpoints.method(UserRelativeCaregiverMessage, DefaultResponseMessage,
+                      path="users/{id}/relatives", http_method="PATCH", name="user.addRelatives")
+    def update_relatives(self, request):
+        RecipexServerApi.authentication_check()
+        user = Key(User, request.id).get()
+        if not user:
+            return DefaultResponseMessage(code="404 Not Found", message="User not existent.")
+
+        relatives = {}
+        if not user.relatives:
+            relatives = user.relatives
+        # TODO Gestire la responsabilità doppia sui dizionari (Paziente-Familiare)
+        for relative in range(len(request.set)):
+            # If already present, means remove
+            if relative in relatives:
+                del relatives[relative]
+            # If not present, means add
+            else:
+                relative_key = Key(User, relative)
+                if not relative_key.get():
+                    return DefaultResponseMessage(code="404 Not Found", message="User(s) not existent.")
+                relatives[relative] = relative_key
+
+        user.relatives = relatives
+        return DefaultResponseMessage(code="200 OK", message="Relatives updated.")
+
+    @endpoints.method(UserRelativeCaregiverMessage, DefaultResponseMessage,
+                      path="users/{id}/relatives", http_method="PATCH", name="user.addRelatives")
+    def update_caregivers(self, request):
+        RecipexServerApi.authentication_check()
+        user = Key(User, request.id).get()
+        if not user:
+            return DefaultResponseMessage(code="404 Not Found", message="User not existent.")
+
+        caregivers = {}
+        if not user.caregivers:
+            caregivers = user.caregivers
+        # TODO Gestire la responsabilità doppia trai dizionari (Paziente-Caregiver)
+        for caregiver in range(len(request.set)):
+            # If already present, means remove
+            if caregiver in caregivers:
+                del caregivers[caregiver]
+            # If not present, means add
+            else:
+                caregiver_key = Key(User, caregiver)
+                if not caregiver_key.get():
+                    return DefaultResponseMessage(code="404 Not Found", message="Caregiver(s) not existent.")
+            caregivers[caregiver] = caregiver_key
+
+        user.caregivers = caregivers
+        return DefaultResponseMessage(code="200 OK", message="Relatives updated.")
 
     @endpoints.method(AddMeasurementMessage, DefaultResponseMessage,
                       path="users/{user_id}/measurements", http_method="POST", name="measurements.addMeasurement")
@@ -365,8 +439,9 @@ class RecipexServerApi(remote.Service):
         return DefaultResponseMessage(code="201 Created", message="Measurement added.", payload=measurement_key.id())
 
     @endpoints.method(UpdateMeasurementMessage, DefaultResponseMessage,
-                      path="users/{user_id}/measurements/{id}", http_method="PUT", name="measurements.updateMeasurement")
+                      path="users/{user_id}/measurements/{id}", http_method="PUT", name="measurement.updateMeasurement")
     def update_measurement(self, request):
+        RecipexServerApi.authentication_check()
         measurement = Key(Measurement, request.id).get()
         if not measurement:
             return DefaultResponseMessage(code="404 Not Found", message="Measurement not existent.")
@@ -439,6 +514,42 @@ class RecipexServerApi(remote.Service):
 
         measurement.put()
         return DefaultResponseMessage(code="200 OK", message="Measurement updated.")
+
+    @endpoints.method(MeasurementIdMessage, MeasurementInfoMessage,
+                      path="users/{user_id}/measurements/{id}", http_method="GET", name="measurement.getMeasurement")
+    def get_measurement(self, request):
+        RecipexServerApi.authentication_check()
+        measurement = Key(Measurement, request.id).get()
+        if not measurement:
+            return MeasurementInfoMessage(response=DefaultResponseMessage(code="404 Not Found",
+                                                                          message="Measurement not existent."))
+        user_key = Key(User, request.user_id)
+        if user_key != measurement.parent:
+            return MeasurementInfoMessage(response=DefaultResponseMessage(code="401 Unauthorized",
+                                                                          message="User unauthorized."))
+
+        date_time = datetime.strftime(measurement.date_time)
+
+        return MeasurementInfoMessage(date_time=date_time, kind=measurement.kind, systolic=measurement.systolic,
+                                      diastolic=measurement.diastolic, bpm=measurement.bpm, spo2=measurement.spo2,
+                                      respirations=measurement.respirations, degrees=measurement.degrees,
+                                      hgt=measurement.hgt, nrs=measurement.nrs, ch_level=measurement.ch_level,
+                                      response=DefaultResponseMessage(code="200 OK",
+                                                                      message="Measurement info retrieved."))
+
+    @endpoints.method(MeasurementIdMessage, DefaultResponseMessage,
+                      path="users/{user_id}/measurements/{id}", http_method="DELETE", name="measurement.deleteMeasurement")
+    def delete_measurement(self, request):
+        RecipexServerApi.authentication_check()
+        measurement = Key(Measurement, request.id).get()
+        if not measurement:
+            return DefaultResponseMessage(code="404 Not Found", message="Measurement not existent.")
+        user_key = Key(User, request.user_id)
+        if user_key != measurement.parent:
+            return DefaultResponseMessage(code="401 Unauthorized", message="User unauthorized.")
+
+        measurement.key.delete()
+        return DefaultResponseMessage(code="200 OK", message="Measurement deleted.")
 
     @classmethod
     def authentication_check(cls):
